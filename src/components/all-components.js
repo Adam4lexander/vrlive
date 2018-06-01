473d4ec53c66664bda467f7ef5f2d2c3c449ae8b
Adam <twite.adam@gmail.com>
2018-06-01 22:34:04 +1000

    trying different html2canvas component

--- src/components/all-components.js
+++ src/components/all-components.js
@@ -2,6 +2,7 @@ import aframe from "aframe";
 import "aframe-state-component";
 import draw from "aframe-draw-component";
 import "aframe-htmltexture-component";
+import "aframe-html-shader";
 import logger from "../lib/log";
 
 aframe.registerComponent("draw", draw.component);
